f953c5a614bf544ed073ce48b5e1eb3bdce3ff3b
wkway <104142595+wkway@users.noreply.github.com>
2022-05-06 23:26:11 -0700

    Update dnscrypt-proxy-config.sh

--- dnscrypt-proxy-config.sh
+++ dnscrypt-proxy-config.sh
@@ -1,7 +1,7 @@
 wget -N https://github.com/felixonmars/dnsmasq-china-list/raw/master/bogus-nxdomain.china.conf
 cat bogus-nxdomain.china.conf | grep -v '^#bogus' | grep bogus-nxdomain | sed 's/bogus-nxdomain=//g' | sed -e 's/$/\r/' >dnscrypt-blacklist-ips.txt
 wget -N https://raw.githubusercontent.com/wkway/dns/master/blacklist-ips/blacklist-ips.txt
-cat blacklist-ips.txt dnscrypt-blacklist-ips.txt | sort | uniq >blocked-ips.txt
+cat blacklist-ips.txt dnscrypt-blacklist-ips.txt | sort | uniq | sed -e 's/$/\r/' >blocked-ips.txt
 
 wget -N https://raw.githubusercontent.com/wkway/dns/master/cloaking-rules/cloaking-rule.txt
 wget -N https://github.com/googlehosts/hosts/raw/master/hosts-files/dnscrypt-proxy-cloaking.txt
@@ -17,5 +17,6 @@ cat adservers.conf >>unbound.blacklist.conf
 awk '!a[$0]++' unbound.blacklist.conf >unbound_ad_servers
 
 #wget -N https://raw.githubusercontent.com/wkway/dns/master/blocked-names/blocked-names.txt
+sed -e 's/$/\r/' >blocked-names.txt
 
 rm bogus-nxdomain.china.conf dnscrypt-blacklist-ips.txt blacklist-ips.txt cloaking-rule.txt dnscrypt-proxy-cloaking.txt tmp.txt accelerated-domains.china.conf adservers.conf unbound.blacklist.conf 
